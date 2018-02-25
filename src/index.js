dde1c12091abe69a33b1df58a3f7dda41aeb4f80
NSpeko <nonstoper42@gmail.com>
2018-02-25 22:23:29 +0300

    Feature: Implemented exchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    console.log(currency);
+    if(currency>10000){
+        return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    else if(currency<=0){
+        return {};
+    }
+    else{
+        let resultObject={};
+        if(Math.floor(currency  / 50)!==0){
+            resultObject.H=Math.floor(currency  / 50);
+            currency-=resultObject.H*50;
+        }
+        if(Math.floor(currency  / 25)!==0){
+            resultObject.Q=Math.floor(currency  / 25);
+            currency-=resultObject.Q*25;
+        }
+        if(Math.floor(currency  / 10)!==0){
+            resultObject.D=Math.floor(currency  / 10);
+            currency-=resultObject.D*10;
+        }
+        if(Math.floor(currency  / 5)!==0){
+            resultObject.N=Math.floor(currency  / 5);
+            currency-=resultObject.N*5;
+        }
+        if(Math.floor(currency%10)!==0){
+            resultObject.P=Math.floor(currency%10);
+        }
+        return resultObject;
+    }
+};
